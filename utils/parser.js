3bc5e57b24d583ac15063239ea637024b82e9efb
jwaher-x <jawaher.5alidfnn@gmail.com>
2026-04-01 13:40:08 +0300

    todo 5

---
+++ utils/parser.js
@@ -0,0 +1,9 @@
+import lodsh from "lodash"
+export function parseNumbers(input) {
+  const numbers = _.map(input, (str) => Number(str));
+  return _.compact(numbers);
+}
+export function isValidOperation(operation) {
+    const validOps = ["add", "subtract", "multiply", "divide"];
+    return _.includes(validOps, operation);
+  }
