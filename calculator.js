711285ee6b0ef11939f171e78f4732233b5bda23
jwaher-x <jawaher.5alidfnn@gmail.com>
2026-04-01 13:32:05 +0300

    todo 3

--- calculator.js
+++ calculator.js
@@ -217,3 +217,25 @@ import { parseNumbers, isValidOperation } from "./utils/parser.js";
 import _ from "lodash";
 const operation = process.argv[2];
 const numbers = process.argv.slice(3);
+if (!isValidOperation(operation)){
+  console.log("Invalid operation. Use: add, subtract, multiply, or divide")
+}
+const nums = parseNumbers(numbers);
+  let result;
+
+  switch (operation) {
+  case "add":
+    result = add(nums);
+    break;
+  case "subtract":
+    result = subtract(nums);
+    break;
+  case "multiply":
+    result = multiply(nums);
+    break;
+  case "divide":
+    result = divide(nums);
+    break;
+}
+console.log(`Result: ${result}`);
+
